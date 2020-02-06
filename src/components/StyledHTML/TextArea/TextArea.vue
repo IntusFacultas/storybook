1ae9e0079e99482171a42c52ca46606fe73d46e0
Pedro Del Moral Lopez <pdelmorallopez@gmail.com>
2020-02-06 18:01:33 -0500

    Update TextArea.vue

--- src/components/StyledHTML/TextArea/TextArea.vue
+++ src/components/StyledHTML/TextArea/TextArea.vue
@@ -3,6 +3,7 @@
     <n-label :dark="labelDark" :flavor="labelFlavor" :for="name">{{label}}</n-label>
     <n-text-area
       :id="name"
+      :flavor="flavor"
       :cols="cols"
       :rows="rows"
       :readonly="readonly"
@@ -11,23 +12,74 @@
       :maxlength="maxlength"
       :required="required"
       :disabled="disabled"
-      :flavor="flavor"
       :autofocus="autofocus"
       v-model="value"
       @input="oninput"
+      @change="onChange"
+      @focus="onFocus"
     ></n-text-area>
   </div>
 </template>
 
 <script>
-import { NLabel } from "Components/components/StyledHTML/Typography/Typography.vue";
+import { NLabel } from "@intus/typography";
 import Theme from "@intus/designsystem";
 import styled from "vue-styled-components";
 require("@intus/fonts");
 const props = {
   flavor: {
     type: String,
-    default: "Info"
+    default: "LightBlue"
+  },
+  defaultTheme: {
+    type: Object,
+    default: function() {
+      return Theme;
+    }
+  }
+};
+export const NTextArea = styled("textarea", props)`
+  width: 100%;
+  height: 35px;
+  font-size: 16px;
+  border-radius: 5px;
+  border-width: 1px;
+  border-style: solid;
+  padding: 2px 5px 2px 5px;
+  margin-top: 2px;
+  box-sizing: border-box;
+  <template>
+  <div class="input-container">
+    <n-label :dark="labelDark" :flavor="labelFlavor" :for="name">{{label}}</n-label>
+    <n-text-area
+      :id="name"
+      :flavor="flavor"
+      :cols="cols"
+      :rows="rows"
+      :readonly="readonly"
+      :placeholder="placeholder"
+      :name="name"
+      :maxlength="maxlength"
+      :required="required"
+      :disabled="disabled"
+      :autofocus="autofocus"
+      v-model="value"
+      @input="oninput"
+      @change="onChange"
+      @focus="onFocus"
+    ></n-text-area>
+  </div>
+</template>
+
+<script>
+import { NLabel } from "@intus/typography";
+import Theme from "@intus/designsystem";
+import styled from "vue-styled-components";
+require("@intus/fonts");
+const props = {
+  flavor: {
+    type: String,
+    default: "LightBlue"
   },
   defaultTheme: {
     type: Object,
@@ -88,6 +140,10 @@ export const VueTextArea = {
     };
   },
   props: {
+    flavor: {
+      type: String,
+      default: "LightBlue"
+    },
     autofocus: {
       type: Boolean,
       default: false
@@ -135,10 +191,6 @@ export const VueTextArea = {
     disabled: {
       type: Boolean,
       default: false
-    },
-    flavor: {
-      type: String,
-      default: "Info"
     }
   },
   mounted: function() {
@@ -152,6 +204,134 @@ export const VueTextArea = {
     oninput: function() {
       var self = this;
       self.$emit("input", self.value);
+    },
+    onChange() {
+      this.$emit("change", this.value);
+    },
+    onFocus() {
+      this.$emit("focus");
+    }
+  }
+};
+export default VueTextArea;
+</script>
+
+<style>
+.input-container {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+}
+</style>
+  transition: box-shadow 0.5s cubic-bezier(0, 0.99, 0.37, 1.01);
+  &:focus {
+    border-color: ${props =>
+      props.theme && props.theme[props.flavor]
+        ? props.theme[props.flavor].border.color
+        : props.defaultTheme[props.flavor] &&
+          props.defaultTheme[props.flavor].border.color
+        ? props.defaultTheme[props.flavor].border.color
+        : "#040404"};
+    outline: none;
+    box-shadow: 0px 0px 0px 3px
+      ${props =>
+        props.theme && props.theme[props.flavor]
+          ? props.theme[props.flavor].border.color
+          : props.defaultTheme[props.flavor] &&
+            props.defaultTheme[props.flavor].border.color
+          ? props.defaultTheme[props.flavor].border.color
+          : "#040404"};
+  }
+  &:read-only {
+    background-color: #e9e9e9;
+    color: #747474;
+  }
+  &:disabled {
+    cursor: not-allowed;
+    pointer-events: none;
+    color: #747474;
+    background-color: #e2e2e2;
+  }
+`;
+export const VueTextArea = {
+  name: "vue-text-area",
+  components: { NTextArea, NLabel },
+  data: function() {
+    return {
+      value: ""
+    };
+  },
+  props: {
+    flavor: {
+      type: String,
+      default: "LightBlue"
+    },
+    autofocus: {
+      type: Boolean,
+      default: false
+    },
+    readonly: {
+      type: Boolean,
+      default: false
+    },
+    placeholder: {
+      type: String,
+      default: ""
+    },
+    maxlength: {
+      type: String,
+      default: ""
+    },
+    cols: {
+      type: String,
+      default: ""
+    },
+    rows: {
+      type: String,
+      default: ""
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    required: {
+      type: Boolean,
+      default: false
+    },
+    labelFlavor: {
+      type: String,
+      default: ""
+    },
+    labelDark: {
+      type: Boolean,
+      default: false
+    },
+    label: {
+      type: String,
+      required: true
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  mounted: function() {
+    var self = this;
+    if (typeof self.$parent !== "undefined") {
+      if (!self.$parent.$refs.inputs) self.$parent.$refs.inputs = {};
+      self.$parent.$refs.inputs[self.name] = self;
+    }
+  },
+  methods: {
+    oninput: function() {
+      var self = this;
+      self.$emit("input", self.value);
+    },
+    onChange() {
+      this.$emit("change", this.value);
+    },
+    onFocus() {
+      this.$emit("focus");
     }
   }
 };
