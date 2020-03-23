29308ec53c73cbdba606d6d13f52a4cbcde551e6
Pedro Del Moral Lopez <pdelmorallopez@gmail.com>
2020-03-22 21:51:46 -0400

    Removed NIWS

--- src/components/NIWSReviewer.stories.js
+++ src/components/NIWSReviewer.stories.js
@@ -1,49 +1,51 @@
-import { withA11y } from '@storybook/addon-a11y';
-import { withKnobs, object, text, } from '@storybook/addon-knobs';
+import { withA11y } from "@storybook/addon-a11y";
+import { withKnobs, object, text } from "@storybook/addon-knobs";
 import { NiwsReviewer } from "Components/components/NIWSReviewer/niwsReviewer.vue";
 import { NiwsStackedReviewer } from "Components/components/NIWSReviewer/niwsStackedReviewer.vue";
 import markdown from "Components/components/NIWSReviewer/USAGE.md";
 
 export default {
-    title: 'NIWS/Reviewing Panel',
-    decorators: [withA11y, withKnobs],
-    parameters: {
-        notes: { markdown }
-    },
-    // Our exports that end in "Data" are not stories.
-    excludeStories: /.*Data$/,
+  title: "NIWS/Reviewing Panel",
+  decorators: [withA11y, withKnobs],
+  parameters: {
+    notes: { markdown }
+  },
+  // Our exports that end in "Data" are not stories.
+  excludeStories: /.*Data$/
 };
 
 const propData = {
-    transitions: {
-        default: object("NIWS Transitions", {})
-    },
-    instanceId: {
-        default: text("Instance ID", "")
-    },
-    workflowUrl: {
-        default: text("Workflow URL", "")
-    },
-    workflowId: {
-        default: text("Workflow ID", "")
-    },
-    callback: {
-        default: object("Callback Function", function (data) { console.log(data) })
-    }
-}
+  transitions: {
+    default: object("NIWS Transitions", {})
+  },
+  instanceId: {
+    default: text("Instance ID", "")
+  },
+  workflowUrl: {
+    default: text("Workflow URL", "")
+  },
+  workflowId: {
+    default: text("Workflow ID", "")
+  },
+  callback: {
+    default: object("Callback Function", function(data) {
+      console.log(data);
+    })
+  }
+};
 
-export const HorizontalReviewer = () => ({
-    components: { NiwsReviewer },
-    props: propData,
-    template: `
-        <niws-reviewer></niws-reviewer>
-    `
-});
+// export const HorizontalReviewer = () => ({
+//     components: { NiwsReviewer },
+//     props: propData,
+//     template: `
+//         <niws-reviewer></niws-reviewer>
+//     `
+// });
 
-export const StackedReviewer = () => ({
-    components: { NiwsStackedReviewer },
-    props: propData,
-    template: `
-        <niws-stacked-reviewer></niws-stacked-reviewer>
-    `
-})
+// export const StackedReviewer = () => ({
+//     components: { NiwsStackedReviewer },
+//     props: propData,
+//     template: `
+//         <niws-stacked-reviewer></niws-stacked-reviewer>
+//     `
+// })
